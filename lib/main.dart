a7adf6758057752c86a5d3927a32e4214858e832
AbdurRahmanasim <abdurrahmanasim7@gmail.com>
2024-05-05 23:04:56 +0500

    in progress

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:bottomnav/MainScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MainScreen(),
+    );
+  }
+}
